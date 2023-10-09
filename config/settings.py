cbba91914ee749018040217842d93a25c94df1fc
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-10-09 14:48:38 +0330

    (create Expense models and admin)

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+
+THIRD_PARTY_APPS = [
+]
+
+MY_APPS = [
+    'index.apps.IndexConfig',
+]
+
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+INSTALLED_APPS = THIRD_PARTY_APPS + MY_APPS + DJANGO_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- index/models.py
+++ index/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=225)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
